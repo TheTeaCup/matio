2c944fc18275081d47354c7acd310325de9bb33b
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2018-09-10 16:05:09 -0400

    Update eris.js

--- eris.js
+++ eris.js
@@ -1,33 +1,7 @@
-const Eris = require('eris');
-const auth = require('./auth');
-const bot = new Eris(auth.token);
-let prefix = "m!";
+
+const prefix = "m!";
 
 
-bot.on('messageCreate', function(message) {
-    if (message.content == '!ping') {
-        message.channel.createMessage('Pong!');
-      }
-});
-
-bot.on("ready", () => {
-    console.log("Ready!");
-    
-    const games = [{
-     
-        game: `over ${bot.users.toLocaleString()} users // m!help // ${bot.guilds.size.toLocaleString()} Guild${bot.guilds.size > 1 ? "s" : ""}`,
-        type: 3
-      }];
-       
-   
-
-        bot.editStatus('dnd', {
-            name: games,
-              
-            type: games.type,
-            url: games.url ? games.url : null
-          });
-});
 const Base = require('eris-sharder').Base;
 class Class extends Base{
     constructor(bot) {
@@ -35,11 +9,31 @@ class Class extends Base{
     }
 
     launch() {
-
+		/*
+		 * this occurs after the "ready" event, so you just do whatever you would usually do at the start of launch()
+		 * the bot instance is a property of the Base class, so access it with "this.bot"
+		 */
+		console.log("Ready!");
+		const games = [
+			{
+				game: `over ${bot.users.toLocaleString()} users // m!help // ${bot.guilds.size.toLocaleString()} Guild${bot.guilds.size > 1 ? "s" : ""}`,
+				type: 3
+			}
+		];
+		
+		this.bot.editStatus('dnd', {
+			name: games,
+			type: games.type,
+			url: games.url ? games.url : null
+		});
+		
+		this.bot.on('messageCreate', function(message) {
+			if (message.content == '!ping') {
+				message.channel.createMessage('Pong!');
+			}
+		});
+		
     }
 
 }
-
 module.exports = Class;
-
-bot.connect();
